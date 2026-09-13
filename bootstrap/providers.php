d3f924cb5193ef6cba6cd68d2000ada5d0b75f8f
rjcontrerasm <49208549+rjcontrerasm@users.noreply.github.com>
2026-09-13 00:11:33 -0500

    Enable collaboration service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\AuditServiceProvider::class,
+    App\Providers\CollaborationServiceProvider::class,
 ];
