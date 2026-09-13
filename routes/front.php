d041acca311495abcf53414f480c365bcb48bdb5
rjcontrerasm <49208549+rjcontrerasm@users.noreply.github.com>
2026-09-13 13:35:33 -0500

    Register Services FRONT routes

--- routes/front.php
+++ routes/front.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ClientOpsActionController;
 use App\Http\Controllers\ClientOpsController;
 use App\Http\Controllers\ProjectFrontActionController;
 use App\Http\Controllers\ProjectFrontController;
+use App\Http\Controllers\ServiceOrderFrontActionController;
+use App\Http\Controllers\ServiceOrderFrontController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function (): void {
@@ -41,4 +43,24 @@ Route::middleware('auth')->group(function (): void {
         '/proyectos/{project}/editar',
         [ProjectFrontActionController::class, 'update'],
     )->name('project-front.update');
+
+    Route::get(
+        '/servicios/nuevo',
+        [ServiceOrderFrontController::class, 'create'],
+    )->name('service-order-front.create');
+
+    Route::post(
+        '/servicios',
+        [ServiceOrderFrontActionController::class, 'store'],
+    )->name('service-order-front.store');
+
+    Route::get(
+        '/servicios/{serviceOrder}/editar',
+        [ServiceOrderFrontController::class, 'edit'],
+    )->name('service-order-front.edit');
+
+    Route::post(
+        '/servicios/{serviceOrder}/editar',
+        [ServiceOrderFrontActionController::class, 'update'],
+    )->name('service-order-front.update');
 });
