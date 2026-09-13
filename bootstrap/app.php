18e5058b75cfda90fe45ab4c991f315168f7ef03
rjcontrerasm <49208549+rjcontrerasm@users.noreply.github.com>
2026-09-13 12:56:23 -0500

    Load dedicated front routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function (): void {
+            Route::middleware('web')
+                ->group(base_path('routes/front.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
