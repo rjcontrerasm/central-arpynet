56247b0597fd7db529e7a4c48e25006a3d1355a5
rjcontrerasm <49208549+rjcontrerasm@users.noreply.github.com>
2026-09-24 01:04:58 -0500

    2.39.0: agregar cargador común de assets operacionales

---
+++ resources/views/components/operational-assets.blade.php
@@ -0,0 +1,10 @@
+@once
+<link
+    rel="stylesheet"
+    href="{{ asset('central-assets/operational.css') }}?v=2.39.0"
+>
+<script
+    src="{{ asset('central-assets/operational.js') }}?v=2.39.0"
+    defer
+></script>
+@endonce
