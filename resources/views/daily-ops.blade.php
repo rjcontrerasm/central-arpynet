3d041307d9267ada49bfa8d0b774598e39db74c7
rjcontrerasm <49208549+rjcontrerasm@users.noreply.github.com>
2026-09-25 14:02:34 -0500

    Remove inline spacing style from Mi Día

--- resources/views/daily-ops.blade.php
+++ resources/views/daily-ops.blade.php
@@ -903,7 +903,7 @@
                 </div>
 
                 @if ($projectsAttentionCount > $projectsAttention->count())
-                    <div class="meta" style="margin-top:8px">
+                    <div class="meta meta-spaced">
                         Mostrando {{ $projectsAttention->count() }}
                         de {{ $projectsAttentionCount }} proyecto(s)
                         con señales operativas.
